db2d95281ff6a287cc45bab74c93d9c7ccebd349
setyap <setyap77@gmail.com>
2021-02-07 16:01:30 +0700

    selesai

--- app/src/main/java/com/example/creditwahana/API/RetroServer.java
+++ app/src/main/java/com/example/creditwahana/API/RetroServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String baseurl = "http:/192.168.1.6//wanda/";
+    private static final String baseurl = "http:/192.168.1.6//webservice/";
     private static Retrofit retro;
 
     public static Retrofit konekRetrofit(){
